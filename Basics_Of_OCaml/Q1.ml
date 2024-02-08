3b12dd2d442c4882dcb4ae9518b03336db2235cf
Debashis7307 <debashisalak@gmail.com>
2024-02-08 23:23:07 +0530

    add another two qutstions from this same excercise

--- Basics_Of_OCaml/Q1.ml
+++ Basics_Of_OCaml/Q1.ml
@@ -1,4 +1,4 @@
 
-7*(1+2+3);;
+let (a:int)= 7*(1+2+3);;
 
-"cs "^ string_of_int(3110);;
+let (b:string)="cs "^ string_of_int(3110);;
---
+++ Basics_Of_OCaml/Q3.ml
@@ -0,0 +1,4 @@
+
+let _ = 42 = 42  (* another way to write that would be [let _ = (42 = 42)] *)
+let _ = "hi" = "hi"  (* result is true *)
+let _ = "hi" == "hi" (* result is false *)
